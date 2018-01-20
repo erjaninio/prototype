2541241c45618b46c983324502a6f802a4850ede
erjaninio <35621278+erjaninio@users.noreply.github.com>
2018-01-20 12:32:33 +0600

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image image = new Image();
+            Image clonedImage = image.Clone();
+            image.GetInfo();
+            clonedImage.GetInfo();
+            Console.Read();
+        }
+
+        public class Image
+        {
+            string name;
+            long size;
+            string format;
+
+            public Image()
+            {
+                name = "картинка";
+                size = 13;
+                format = ".png";
+            }
+
+            public void GetInfo()
+            {
+                Console.WriteLine($"Название - {name}, Размер - {size} кБ, Формат - {format}");
+            }
+            public Image Clone()
+            {
+                return new Image();
+            }
+        }
+    }
+}
